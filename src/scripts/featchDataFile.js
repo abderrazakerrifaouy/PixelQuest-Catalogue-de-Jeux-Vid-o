4a5d344e91ac40dfecda8b82109af8da87c75250
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2025-11-14 09:13:38 +0100

    create methode rocherche and animation the carte

--- src/scripts/featchDataFile.js
+++ src/scripts/featchDataFile.js
@@ -12,3 +12,18 @@ export async function FeatchData(url) {
         console.error("Erreur:", error);
     }
 }
+
+
+export async function rochercheById(id){
+   try {
+       const response = await fetch(`https://debuggers-games-api.duckdns.org/api/games/${id}`);
+
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return await response.json();
+
+    } catch (error) {
+        console.error("Erreur:", error);
+    }
+}
